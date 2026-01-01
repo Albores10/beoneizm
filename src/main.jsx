35fce497519e98a120a493e17d1889f13838b50f
Albores10 <birolsafak1@gmail.com>
2026-01-01 15:28:42 +0300

    Fix: Move ErrorBoundary to root level to catch App crashes

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,12 @@ import './index.css'
 import './styles/global.css'
 import App from './App.jsx'
 
+import ErrorBoundary from './components/UI/ErrorBoundary'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </StrictMode>,
 )
